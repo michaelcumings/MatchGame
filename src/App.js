92312541ba0840906042aa10036264f068708b5b
Michael Cumings <michael.cumings@gmail.com>
2018-12-04 04:36:00 -0600

    images

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import cards from "./cards.json";
+
 
 class App extends Component {
+  // Setting this.state.cards to the cards json array
+  state = {
+    cards
+  };
+
+  removeFriend = id => {
+    // Filter this.state.cards for cards with an id not equal to the id being removed
+    const cards = this.state.cards.filter(friend => friend.id !== id);
+    // Set this.state.cards equal to the new cards array
+    this.setState({ cards });
+  };
+
+  // Map over this.state.cards and render a FriendCard component for each friend object
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+        <Title>Clicky Game for Android IDs</Title>
+
+        {this.state.cards.map(friend => (
+          <FriendCard
+            removeCard={this.removeCard}
+            id={friend.id}
+            key={friend.id}
+            image={friend.image}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
